dd2da3edd7df645b879ef4bfcb6a57a5fc6e906f
João Paulo Peres Martins <>
2020-05-12 09:19:29 -0300

    First application with hooks

--- src/App.js
+++ src/App.js
@@ -1,63 +1,39 @@
-import React, {Component} from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
 
-class App extends Component {
-  state = {
+const App = props => {
+  const [ personsState, setPersonsState ] = useState({
     persons: [
       {name: "João Paulo", age: 25},
       {name: "Marina", age: 20},
       {name: "Julia", age: 21}
-    ]
-  }
+    ],
+    otherState: 'other value'
+  });
 
-  updateNameState = () => {
-    //console.log("foi clicado!");
-    //Não fazer isso: this.state.persons[0].name = 'Maximiliam";
-    this.setState( {
+  const updateNameState = () => {
+    setPersonsState( {
       persons: [
         {name: "Carlos", age: 45},
         {name: "Graciana", age: 40},
         {name: "Leonardo", age: 41}
       ]
     });
-  }
+  };
 
-  render() {
-    return (
-      <div className="App">
-        <h1> Hello World!!! </h1>
-        <p> This is really good! </p>
-        <button onClick={this.updateNameState}> Update data </button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>Meus Hobbies: Futebol</Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
-      </div>
-    );
-    // html não é JSX
-    //return React.createElement('div', null, React.createElement('h1', {className: 'App'}, 'Isso está funcionando'));
-
-    /*return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );*/
-  }
+  return (
+    <div className="App">
+      <h1> Hello World!!! </h1>
+      <p> This is really good! </p>
+      <button onClick={updateNameState}> Update data </button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>Meus Hobbies: Futebol</Person>
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}></Person>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}></Person>
+    </div>
+  );
 }
 
 export default App;
